b43be9924a9e463a85e38e9d381dbe07e210a591
cjwaldron18 <cjwaldron18@gmail.com>
2016-11-27 19:49:56 -0500

    Update configuration.cc
    Removing vertical white space; formatting comments;

--- configuration.cc
+++ configuration.cc
@@ -14,13 +14,11 @@
 **/
 Configuration::Configuration() {
 }
-
 /******************************************************************************
  * Destructor.
 **/
 Configuration::~Configuration() {
 }
-
 /******************************************************************************
  * Accessors and mutators.
 **/
@@ -34,13 +32,10 @@ Configuration::~Configuration() {
 int Configuration::GetMaxServiceSubscript() const {
   // Test if function is executed. 
   // Utils::log_stream << kTag << "FUNC: " << "GetMaxServiceSubscript() " 
-  //                   << "EXECUTED." << endl;
- 
+  //                   << "EXECUTED." << endl 
   int value = (actual_service_times_.size()) - 1;
   return value;
 }
-
-
 /******************************************************************************
  * General functions.
 **/
@@ -58,13 +53,11 @@ void Configuration::ReadConfiguration(Scanner& instream) {
   // Test if function is executed. 
   // Utils::log_stream << kTag << "FUNC: " << "ReadConfiguration() "
   //                   << "EXECUTED." << endl; 
-
   string line;
   ScanLine scanline;
-
   line = instream.NextLine();
-  scanline.OpenString(line);  //Takes one line, seperates it by delim, saves 
-  seed_ = scanline.NextInt(); //variables from that one line
+  scanline.OpenString(line);  // Takes one line, seperates it by delimeter,
+  seed_ = scanline.NextInt(); // saves variables from that one line.
   election_day_length_hours_ = scanline.NextInt();
   election_day_length_seconds_ = election_day_length_hours_ * 3600;
   time_to_vote_mean_seconds_ = scanline.NextInt();
@@ -72,23 +65,20 @@ void Configuration::ReadConfiguration(Scanner& instream) {
   max_expected_to_simulate_ = scanline.NextInt();
   wait_time_minutes_that_is_too_long_ = scanline.NextInt();
   number_of_iterations_ = scanline.NextInt();
-
   line = instream.NextLine();
-  scanline.OpenString(line); //Similar to above, split and save data. 
+  scanline.OpenString(line); // Similar to above, split and save data. 
   arrival_zero_ = scanline.NextDouble();
   for (int sub = 0; sub < election_day_length_hours_; ++sub) {
     double input = scanline.NextDouble();
     arrival_fractions_.push_back(input);
   }
-
-  Scanner service_times_file; //Uses actual data for comparision 
+  Scanner service_times_file; // Uses actual data for comparison.
   service_times_file.OpenFile("dataallsorted.txt");
   while (service_times_file.HasNext()) {
     int thetime = service_times_file.NextInt();
     actual_service_times_.push_back(thetime);
   }
 }
-
 /******************************************************************************
  * Function 'ToString'.
  *
@@ -102,44 +92,36 @@ string Configuration::ToString() {
   // Test if function is executed. 
   // Utils::log_stream << kTag << "FUNC: " << "ToString() "
   //                   << "EXECUTED." << endl;
-  
   string s = "\n";
   int offset = 6;
   s += kTag;
-  // s += "RN seed:              ";
+  // s += "RN seed:              "; 
   s += Utils::Format("RN seed:", 22);
   s += Utils::Format(seed_, 8) + "\n";
-  s += kTag;
-  
+  s += kTag;  
   s += "Election Day length:  ";
   s += Utils::Format(election_day_length_seconds_, 8) + " =";
   // s += Utils::Format(election_day_length_seconds_/3600.0, 8, 2) + " (";
   s += Utils::Format(election_day_length_hours_, 6, 2) + " (";
   s += Utils::Format(election_day_length_hours_, 8, 2) + ") hours\n";
-  s += kTag;
-  
+  s += kTag; 
   s += "Time to vote mean:  ";
   s += Utils::Format(time_to_vote_mean_seconds_, 8) + " =";
   s += Utils::Format(time_to_vote_mean_seconds_/60.0, 5, 2) + " minutes\n";
-  s += kTag;
-  
+  s += kTag;  
   s += "Min and max expected voters for this simulation:     ";
   s += Utils::Format(min_expected_to_simulate_, 8);
-  s += Utils::Format(max_expected_to_simulate_, 8) + "\n";
-  
+  s += Utils::Format(max_expected_to_simulate_, 8) + "\n";  
   s += "Wait time (minutes) that is 'too long': ";
-  s += Utils::Format(wait_time_minutes_that_is_too_long_, 8) + "\n";
-  
+  s += Utils::Format(wait_time_minutes_that_is_too_long_, 8) + "\n";  
   s += "Number of iterations to perform: ";
-  s += Utils::Format(number_of_iterations_, 4) + "\n";
-  
+  s += Utils::Format(number_of_iterations_, 4) + "\n";  
   s += "Max service time subscript: ";
   s += Utils::Format(GetMaxServiceSubscript(), 6) + "\n";
   // int offset = 6; Duplicate Declaration
   s += kTag;
   s += Utils::Format(0, 2) + "-" + Utils::Format(0, 2);
   s += " : " + Utils::Format(arrival_zero_, 7, 2) + "\n";
-  
   for (UINT sub = 0; sub < arrival_fractions_.size(); ++sub) {
     s += kTag;
     s += Utils::Format(offset+sub, 2) + "-" + Utils::Format(offset+sub+1, 2);
@@ -148,4 +130,3 @@ string Configuration::ToString() {
   s += "\n";
   return s;
 }
-
