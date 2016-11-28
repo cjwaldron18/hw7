d63cbc5c320a08a71b34ac6edc6fcc29fa165faa
cjwaldron18 <cjwaldron18@gmail.com>
2016-11-27 21:55:54 -0500

    Update onepct.h

--- onepct.h
+++ onepct.h
@@ -36,56 +36,55 @@ public:
 **/
  OnePct();
  virtual ~OnePct();
- // Commented out 27 November 2016, C.J. Waldron
- // Description: The parameterized constructor is not used.
- // OnePct(Scanner& infile);
+// Commented out 27 November 2016, C.J. Waldron
+// Description: The parameterized constructor is not used.
+// OnePct(Scanner& infile);
 
 /******************************************************************************
  * Accessors and Mutators.
 **/
-  int GetExpectedVoters() const;
-  int GetPctNumber() const;
+ int GetExpectedVoters() const;
+ int GetPctNumber() const;
  
 /******************************************************************************
  * General functions.
 **/
-  void ReadData(Scanner& infile);
-  void RunSimulationPct(const Configuration& config, MyRandom& random, 
+ void ReadData(Scanner& infile);
+ void RunSimulationPct(const Configuration& config, MyRandom& random, 
                         ofstream& out_stream);
-
-  string ToString();
-  // Commented out 27 November 2016, C.J. Waldron
-  // Description: This function is not executed.
-  // string ToStringVoterMap(string label, multimap<int, OneVoter> themap);
+ string ToString();
+// Commented out 27 November 2016, C.J. Waldron
+// Description: This function is not executed.
+// string ToStringVoterMap(string label, multimap<int, OneVoter> themap);
 
 private:
-  int    pct_expected_voters_ = kDummyInt;
-  int    pct_expected_per_hour_ = kDummyInt;
-  double pct_minority_ = kDummyDouble;
-  string pct_name_ = kDummyString;
-  int    pct_number_ = kDummyInt;
-  double pct_turnout_ = kDummyDouble;
-  int    pct_stations_ = kDummyInt;
-  int    pct_num_voters_ = kDummyInt;
-  double wait_dev_seconds_;
-  double wait_mean_seconds_;
-  set<int> stations_to_histo_;
-  vector<int> free_stations_;
-  multimap<int, OneVoter> voters_backup_;
-  multimap<int, OneVoter> voters_done_voting_;
-  multimap<int, OneVoter> voters_pending_;
-  multimap<int, OneVoter> voters_voting_;
+ int    pct_expected_voters_ = kDummyInt;
+ int    pct_expected_per_hour_ = kDummyInt;
+ double pct_minority_ = kDummyDouble;
+ string pct_name_ = kDummyString;
+ int    pct_number_ = kDummyInt;
+ double pct_turnout_ = kDummyDouble;
+ int    pct_stations_ = kDummyInt;
+ int    pct_num_voters_ = kDummyInt;
+ double wait_dev_seconds_;
+ double wait_mean_seconds_;
+ set<int> stations_to_histo_;
+ vector<int> free_stations_;
+ multimap<int, OneVoter> voters_backup_;
+ multimap<int, OneVoter> voters_done_voting_;
+ multimap<int, OneVoter> voters_pending_;
+ multimap<int, OneVoter> voters_voting_;
 
 /******************************************************************************
  * General private functions.
 **/
-  void CreateVoters(const Configuration& config, MyRandom& random,
-                    ofstream& out_stream);
-  int DoStatistics(int iteration, const Configuration& config, 
-                   int station_count,
-                   map<int, int>& map_for_histo, ofstream& out_stream);                  
-  void ComputeMeanAndDev();
-  void RunSimulationPct2(int stations);
+ void CreateVoters(const Configuration& config, MyRandom& random,
+                   ofstream& out_stream);
+ int DoStatistics(int iteration, const Configuration& config, 
+                  int station_count,
+                  map<int, int>& map_for_histo, ofstream& out_stream);                  
+ void ComputeMeanAndDev();
+ void RunSimulationPct2(int stations);
 };
 
 #endif // ONEPCT_H
