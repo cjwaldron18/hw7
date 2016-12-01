3b9168c1381178453862aed6cbf61427c7e94101
cjwaldron18 <cjwaldron18@gmail.com>
2016-12-01 04:10:29 -0500

    Update onevoter.h

--- onevoter.h
+++ onevoter.h
@@ -6,14 +6,11 @@
  * to a voting station.
  * 
  * Author/copyright:  Duncan Buell
- * Used and modified with permission by:
- *   CJ Waldron
- *   Abraham Kahn
+ * Modified and used with permission by: Group 2, 1 December 2016
+ *   Abraham Khan
  *   Thomas O'Hara
  *   Stephen Page
- *   Oneil Clemenson
- *
- * Date: November 2016
+ *   C.J. Waldron
  *
 **/
 
